d2b53eec9ffb6b104dddf1b871d73527a735ba51
Дмитрий Кулешов <retmonhy@gmail.com>
2022-03-16 12:19:49 +0700

    Сверстан блок описания About

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,13 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 import { Wrapper, FlexWrapper } from "./GeneralComp/SuppComponents";
+import vektor from "../images/Vector.svg"
+import pit from '../images/pit.svg'
 
 const ImageWrapper = styled(Wrapper)``;
 const Name = styled(Wrapper)`
     h2 {
-        color: #585CC6;
+        color: #585cc6;
         font-family: "Factor-Bold", sans-serif;
         font-size: 32px;
         line-height: 32px;
@@ -20,7 +22,7 @@ const Description = styled(Wrapper)`
 const BirthDate = styled(Wrapper)`
     span {
         font-family: "Gilroy-Regular", sans-serif;
-        color: #8A8A8A;
+        color: #8a8a8a;
     }
 `;
 const Ul = styled(FlexWrapper)`
@@ -33,24 +35,38 @@ const Ul = styled(FlexWrapper)`
         }
     }
 `;
-const Li = ({ label, text }) => {
-    return (
-        <li>
-            <strong>{label}</strong>: {text}
-        </li>
-    )
-}
+const Li = ({ label, text }) => <li> <strong>{label}</strong>: {text} </li>;
+const LiWithIcon = ({ className, label, text, iconPath }) => 
+    <li className={'withIcon ' + className}> 
+        <FlexWrapper >
+            <FlexWrapper>
+                <strong>{label}:&nbsp;</strong> 
+                <span> {text}</span> 
+            </FlexWrapper>
+            <img src={iconPath} alt="icon"/>
+        </FlexWrapper>    
+    </li>;
+
+const AboutMe = styled.div`
+    line-height: 24px;
+    margin-bottom: 40px;
+    strong {
+        font-family: "Gilroy-Bold", sans-serif;
+    }
+    span {
+        font-family: "Gilroy-Medium", sans-serif;
+    }
+`;
 
 const About = () => {
     return (
-        <Wrapper>
+        <Wrapper className="container">
             <FlexWrapper>
                 <ImageWrapper>
-                    <img/>
+                    {/* <img /> */}
                 </ImageWrapper>
                 <Description>
                     <Wrapper>
-                        
                         <FlexWrapper>
                             <Name>
                                 <h2>Дмитрий Кулешов</h2>
@@ -61,15 +77,32 @@ const About = () => {
                         </FlexWrapper>
 
                         <Ul as="ul">
-                            <Li label="Город" text="Томск"/>
-                            <Li label="Пол" text="мужчина"/>
-                            <Li label="Возраст" text="21"/>
+                            <Li label="Город" text="Томск" />
+                            <LiWithIcon className="iconRight" label="Пол" text="мужчина" iconPath={vektor}/>
+                            <Li label="Возраст" text="21" />
                         </Ul>
+                        <AboutMe>
+                            <strong>О себе: </strong>
+                            <span>
+                                Хэй хэй хэй! Меня зовут Дмитрий, но друзья меня
+                                называют просто - Дмитрий. Мне 21 год и я
+                                обучаюсь в магистратуре ТГУ. По окончании
+                                бакалавриата понял, что хочу заниматься другим и
+                                решил начать учиться разработке. Так как был
+                                выбран фронтенд, то начал я с верстки, стилей и
+                                JS. Сейчат активно изучаю React JS. Прошел
+                                курсик на одной из платформ по этой библиотеке.
+                                Сейчас пишу своё небольшое учебное приложение,
+                                для получения новых знаний и оттачивания уже
+                                существующих.
+                            </span>
+                        </AboutMe>
+                        <LiWithIcon as="div" className="iconLeft" label="Домашнее животное" text="есть" iconPath={pit}/>
                     </Wrapper>
                 </Description>
             </FlexWrapper>
         </Wrapper>
     );
-}
+};
 
-export default About
+export default About;
